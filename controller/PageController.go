0a32c7555e1baaf0f41b6c417f6a45e4c20b1d0a
leijiwu <1287279970@qq.com>
2019-07-16 01:40:02 +0800

    config data base

--- controller/PageController.go
+++ controller/PageController.go
@@ -1,35 +1,34 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
 	"../restgo"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"fmt"
 )
 
 type PageController struct {
 	restgo.Controller
 }
 
-
-
-func (ctrl *PageController)before() gin.HandlerFunc {
+func (ctrl *PageController) before() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		uri := ctx.Request.RequestURI
 		fmt.Print(uri)
-		if 1==1{
+		if 1 == 1 {
 			ctx.Next()
 		}
 		return
 	}
 }
 
-func (ctrl *PageController)Router(router *gin.Engine){
-	router.GET("/",ctrl.showIndex)
+func (ctrl *PageController) Router(router *gin.Engine) {
+	router.GET("/", ctrl.showIndex)
 
 }
 
 //展示首页
-func (ctrl * PageController) showIndex(ctx *gin.Context){
-	ctx.HTML(http.StatusOK,"user/login.html","")
+func (ctrl *PageController) showIndex(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "user/login.html", "")
 }
